da45f2cfd0c27f371746ffa2a8bff04cf2055fd2
BrocksiNet <b.meyer@shopware.com>
2026-06-12 16:29:45 +0200

    test: rename Javascript test directory

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'jsdom',
-        include: ['Tests/JavaScript/**/*.test.js'],
+        include: ['Tests/Javascript/**/*.test.js'],
     },
 });
